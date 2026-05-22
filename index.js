6c6710f70fa48c14fc3986f849ade49ebfbec63d
ben-covered <ben.murray@coveredrestoration.com>
2026-05-21 21:39:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,9 +30,7 @@ if (!process.env.DATABASE_URL) {
 // ---------------------------------------------------------------------------
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL.includes('railway.internal')
-    ? false
-    : { rejectUnauthorized: false },
+  ssl: false,
 });
 
 // ---------------------------------------------------------------------------
